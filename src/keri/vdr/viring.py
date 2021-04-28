98a4b140ef8eeda803f6d3cbc97646b456873933
Phil Feairheller <pfeairheller@gmail.com>
2021-04-28 16:36:51 -0400

    TEL Sub-databases: implementation and unit tests
    Signed-off-by: Phil Feairheller <pfeairheller@gmail.com>

--- src/keri/vdr/viring.py
+++ src/keri/vdr/viring.py
@@ -4,11 +4,12 @@ keri.db.viring module
 
 VIR  Verifiable Issuance(Revocation) Registry
 
-Provides public simple Verificable Credential Issuance/Revocation Registry
+Provides public simple Verifiable Credential Issuance/Revocation Registry
 A special purpose Verifiable Data Registry (VDR)
 """
 
 from keri.db import dbing
+
 
 class Issuer(dbing.LMDBer):
     """
@@ -17,11 +18,40 @@ class Issuer(dbing.LMDBer):
     Attributes:
         see superclass LMDBer for inherited attributes
 
-        .virs is named sub DB whose values are VC issuance/revocation state
+
+        .tvts is named sub DB whose values are serialized TEL events
             dgKey
             DB is keyed by identifer prefix plus digest of serialized event
             Only one value per DB key is allowed
-
+        .tels is named sub DB of key event log tables that map sequence numbers
+            to serialized event digests.
+            snKey
+            Values are digests used to lookup event in .tvts sub DB
+            DB is keyed by identifer prefix plus sequence number of tel event
+            Only one value per DB key is allowed
+        .tibs is named sub DB of indexed backer signatures of event
+            Backers always have nontransferable indetifier prefixes.
+            The index is the offset of the backer into the backer list
+            of the most recent establishment event wrt the receipted event.
+            dgKey
+            DB is keyed by identifer prefix plus digest of serialized event
+            More than one value per DB key is allowed
+        .twes is named sub DB of partially backer escrowed event tables
+            that map sequence numbers to serialized event digests.
+            snKey
+            Values are digests used to lookup event in .evts sub DB
+            DB is keyed by identifer prefix plus sequence number of tel event
+            Only one value per DB key is allowed
+        .ancs is a named sub DB of anchors to KEL events.  Quadlet
+            Each quadruple is concatenation of  four fully qualified items
+            of validator. These are: transferable prefix, plus latest establishment
+            event sequence number plus latest establishment event digest,
+            plus indexed event signature.
+            When latest establishment event is multisig then there will
+            be multiple quadruples one per signing key, each a dup at same db key.
+            dgKey
+            DB is keyed by identifer prefix plus digest of serialized event
+            More than one value per DB key is allowed
 
     Properties:
 
@@ -57,7 +87,6 @@ class Issuer(dbing.LMDBer):
         """
         super(Issuer, self).__init__(headDirPath=headDirPath, reopen=reopen, **kwa)
 
-
     def reopen(self, **kwa):
         """
         Open sub databases
@@ -68,5 +97,261 @@ class Issuer(dbing.LMDBer):
         # Names end with "." as sub DB name must include a non Base64 character
         # to avoid namespace collisions with Base64 identifier prefixes.
 
-        self.virs = self.env.open_db(key=b'virs.')
+        self.tvts = self.env.open_db(key=b'tvts.')
+        self.tels = self.env.open_db(key=b'tels.')
+        self.tibs = self.env.open_db(key=b'tibs.', dupsort=True)
+        self.twes = self.env.open_db(key=b'twes.', dupsort=True)
+        self.ancs = self.env.open_db(key=b'ancs.', dupsort=True)
+
+    def putTvt(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.tvts, key, val)
+
+    def setTvt(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.tvts, key, val)
+
+    def getTvt(self, key):
+        """
+        Use dgKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.tvts, key)
+
+    def delTvt(self, key):
+        """
+        Use dgKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.tvts, key)
+
+    def putTel(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.tels, key, val)
+
+    def setTel(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.tels, key, val)
+
+    def getTel(self, key):
+        """
+        Use dgKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.tels, key)
+
+    def delTel(self, key):
+        """
+        Use dgKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.tels, key)
+
+    def getTibs(self, key):
+        """
+        Use dgKey()
+        Return list of indexed witness signatures at key
+        Returns empty list if no entry at key
+        Duplicates are retrieved in lexocographic order not insertion order.
+        """
+        return self.getVals(self.tibs, key)
+
+    def getTibsIter(self, key):
+        """
+        Use dgKey()
+        Return iterator of indexed witness signatures at key
+        Raises StopIteration Error when empty
+        Duplicates are retrieved in lexocographic order not insertion order.
+        """
+        return self.getValsIter(self.tibs, key)
+
+    def putTibs(self, key, vals):
+        """
+        Use dgKey()
+        Write each entry from list of bytes indexed witness signatures vals to key
+        Adds to existing signatures at key if any
+        Returns True If no error
+        Apparently always returns True (is this how .put works with dupsort=True)
+        Duplicates are inserted in lexocographic order not insertion order.
+        """
+        return self.putVals(self.tibs, key, vals)
+
+    def addTib(self, key, val):
+        """
+        Use dgKey()
+        Add indexed witness signature val bytes as dup to key in db
+        Adds to existing values at key if any
+        Returns True if written else False if dup val already exists
+        Duplicates are inserted in lexocographic order not insertion order.
+        """
+        return self.addVal(self.tibs, key, val)
+
+    def cntTibs(self, key):
+        """
+        Use dgKey()
+        Return count of indexed witness signatures at key
+        Returns zero if no entry at key
+        """
+        return self.cntVals(self.tibs, key)
+
+    def delTibs(self, key, val=b''):
+        """
+        Use dgKey()
+        Deletes all values at key if val = b'' else deletes dup val = val.
+        Returns True If key exists in database (or key, val if val not b'') Else False
+        """
+        return self.delVals(self.tibs, key, val)
+
+    def putTwe(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.twes, key, val)
+
+    def setTwe(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.twes, key, val)
+
+    def getTwe(self, key):
+        """
+        Use dgKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.twes, key)
+
+    def delTwe(self, key):
+        """
+        Use dgKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.twes, key)
+
+    def putAncs(self, key, vals):
+        """
+        Use dgKey()
+        Write each entry from list of bytes receipt quadruples vals to key
+        quadruple is spre+ssnu+sdig+sig
+        Adds to existing receipts at key if any
+        Returns True If no error
+        Apparently always returns True (is this how .put works with dupsort=True)
+        Duplicates are inserted in lexocographic order not insertion order.
+        """
+        return self.putVals(self.ancs, key, vals)
+
+
+    def addAnc(self, key, val):
+        """
+        Use dgKey()
+        Add receipt quadruple val bytes as dup to key in db
+        quadruple is spre+ssnu+sdig+sig
+        Adds to existing values at key if any
+        Returns True if written else False if dup val already exists
+        Duplicates are inserted in lexocographic order not insertion order.
+        """
+        return self.addVal(self.ancs, key, val)
+
+
+    def getAncs(self, key):
+        """
+        Use dgKey()
+        Return list of receipt quadruples at key
+        quadruple is spre+ssnu+sdig+sig
+        Returns empty list if no entry at key
+        Duplicates are retrieved in lexocographic order not insertion order.
+        """
+        return self.getVals(self.ancs, key)
+
+
+    def getAncsIter(self, key):
+        """
+        Use dgKey()
+        Return iterator of receipt quadruples at key
+        quadruple is spre+ssnu+sdig+sig
+        Raises StopIteration Error when empty
+        Duplicates are retrieved in lexocographic order not insertion order.
+        """
+        return self.getValsIter(self.ancs, key)
+
+
+    def cntAncs(self, key):
+        """
+        Use dgKey()
+        Return count of receipt quadruples at key
+        Returns zero if no entry at key
+        """
+        return self.cntVals(self.ancs, key)
+
+
+    def delAncs(self, key, val=b''):
+        """
+        Use dgKey()
+        Deletes all values at key if val = b'' else deletes dup val = val.
+        Returns True If key exists in database (or key, val if val not b'') Else False
+        """
+        return self.delVals(self.ancs, key, val)
+
+
+
+def regKey(pre, rdig):
+    """
+    Returns bytes Registry key from concatenation of ':' with qualified Base64 issuer
+    prefix bytes pre and qualified Base64 bytes digest of serialized vcp event
+    If pre or dig are str then converts to bytes
+    """
+    if hasattr(pre, "encode"):
+        pre = pre.encode("utf-8")  # convert str to bytes
+    if hasattr(rdig, "encode"):
+        rdig = rdig.encode("utf-8")  # convert str to bytes
+    return (b'%s:%s' %  (pre, rdig))
+
+def vcKey(pre, rdig, vcdig):
+    """
+    Returns bytes Registry key from concatenation of ':' with qualified Base64 issuer
+    prefix bytes pre and qualified Base64 bytes digest of serialized vcp event
+    If pre or dig are str then converts to bytes
+    """
+    if hasattr(pre, "encode"):
+        pre = pre.encode("utf-8")  # convert str to bytes
+    if hasattr(rdig, "encode"):
+        rdig = rdig.encode("utf-8")  # convert str to bytes
+    if hasattr(vcdig, "encode"):
+        vcdig = vcdig.encode("utf-8")  # convert str to bytes
+    return (b'%s:%s:%s' %  (pre, rdig, vcdig))
 
--- tests/vdr/test_viring.py
+++ tests/vdr/test_viring.py
@@ -3,19 +3,244 @@
 tests.db.dbing module
 
 """
-import pytest
+import json
+import os
 
-from keri.db import dbing
-from keri.vdr import viring
+import lmdb
+
+from keri.core.coring import Diger, Versify, Serials
+from keri.db.dbing import openLMDB, dgKey, snKey
+from keri.vdr.viring import Issuer, regKey, vcKey
+
 
 def test_issuer():
     """
     Test Issuer Class
     """
-    issuer = viring.Issuer()
+    issuer = Issuer()
+
+    assert isinstance(issuer, Issuer)
+    assert issuer.name == "main"
+    assert issuer.temp is False
+    assert isinstance(issuer.env, lmdb.Environment)
+    assert issuer.path.endswith("keri/db/main")
+    assert issuer.env.path() == issuer.path
+    assert os.path.exists(issuer.path)
+
+    assert isinstance(issuer.tvts, lmdb._Database)
+
+    issuer.close(clear=True)
+    assert not os.path.exists(issuer.path)
+    assert not issuer.opened
+
+    # test not opened on init
+    issuer = Issuer(reopen=False)
+    assert isinstance(issuer, Issuer)
+    assert issuer.name == "main"
+    assert issuer.temp is False
+    assert issuer.opened is False
+    assert issuer.path is None
+    assert issuer.env is None
+
+    issuer.reopen()
+    assert issuer.opened
+    assert issuer.path is not None
+    assert isinstance(issuer.env, lmdb.Environment)
+    assert issuer.path.endswith("keri/db/main")
+    assert issuer.env.path() == issuer.path
+    assert os.path.exists(issuer.path)
+
+    issuer.close(clear=True)
+    assert not os.path.exists(issuer.path)
+    assert not issuer.opened
+
+    assert isinstance(issuer.tvts, lmdb._Database)
+
+    with openLMDB(cls=Issuer) as issuer:
+        assert isinstance(issuer, Issuer)
+        assert issuer.name == "test"
+        assert issuer.temp is True
+        assert isinstance(issuer.env, lmdb.Environment)
+        assert issuer.path.startswith("/tmp/keri_lmdb_")
+        assert issuer.path.endswith("_test/keri/db/test")
+        assert issuer.env.path() == issuer.path
+        assert os.path.exists(issuer.path)
+
+        assert isinstance(issuer.tvts, lmdb._Database)
+
+    assert not os.path.exists(issuer.path)
+
+    ipreb = "DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo".encode("utf-8")
+    regb = "EOWdT7a7fZwRz0jiZ0DJxZEM3vsNbLDPEUk-ODnif3O0".encode("utf-8")
+    rarb = "BijzaUuRMwh1ivT5BQrqNhbvx82lB-ofrHVHjL3WADbA".encode("utf-8")
+
+    #  test with registry inception (vcp) event
+    regk = regKey(ipreb, regb)
+    assert regk == b'DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo:EOWdT7a7fZwRz0jiZ0DJxZEM3vsNbLDPEUk-ODnif3O0'
+    sn = 0
+    vs = Versify(kind=Serials.json, size=20)
+
+    vcp = dict(v=vs, i=regk.decode("utf-8"),
+               s="{:x}".format(sn), b=[rarb.decode("utf-8")],
+               t="vcp")
+
+    vcpb = json.dumps(vcp, separators=(",", ":"), ensure_ascii=False).encode("utf-8")
+    assert vcpb == (b'{"v":"KERI10JSON000014_",'
+                    b'"i":"DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo:EOWdT7a7fZwRz0jiZ0DJxZEM3vsNbLDPEUk-ODnif3O0",'
+                    b'"s":"0","b":["BijzaUuRMwh1ivT5BQrqNhbvx82lB-ofrHVHjL3WADbA"],"t":"vcp"}')
+    vdig = Diger(ser=vcpb)
+
+
+    with openLMDB(cls=Issuer) as issuer:
+        key = dgKey(regk, vdig.qb64b)
+
+        assert issuer.getTvt(key) is None
+        assert issuer.delTvt(key) is False
+        assert issuer.putTvt(key, val=vcpb) is True
+        assert issuer.getTvt(key) == vcpb
+        assert issuer.putTvt(key, val=vcpb) is False
+        assert issuer.setTvt(key, val=vcpb) is True
+        assert issuer.getTvt(key) == vcpb
+        assert issuer.delTvt(key) is True
+        assert issuer.getTvt(key) is None
+
+        telKey = snKey(regk, sn)
+        assert issuer.getTel(telKey) is None
+        assert issuer.delTel(telKey) is False
+        assert issuer.putTel(telKey, val=vdig.qb64b)
+        assert issuer.getTel(telKey) == vdig.qb64b
+        assert issuer.putTel(telKey, val=vdig.qb64b) is False
+        assert issuer.setTel(telKey, val=vdig.qb64b) is True
+        assert issuer.getTel(telKey) == vdig.qb64b
+        assert issuer.delTel(telKey) is True
+        assert issuer.getTel(telKey) is None
+
+        coupl01 = ("BPVuWC4Hc0izqPKn2LIwhp72SHJSRgfaL1RhtuiavIy4AAfiKvopJ0O2afOmxb5A6JtdY7Wkl_1uNx1Z8xQkg_"
+                   "gMzf-vTfEHDylFdgn2e_u_ppaFajIdvEvONX6dcSYzlfBQ").encode("utf-8")
+        coupl02 = ("BW1gbapuOJ4TJKwLfKZs5cXEIs9k8EtBqxR1psVxnD7IABrSkjrgPGXdhBiOy6LUZpiqtsHkKHhfLGj_LhT1n6"
+                   "EqCIdDjrihzrdM1bm0ZNJDwbDGXoeeZujd7ZYsOsBPzRCw").encode("utf-8")
+        coupl03 = ("BklrMm7GlYzNrPQunLJHFn_1wWjlUslGkXfs0KyoNOEAAC_6PB5Zre_E_7YLkM9OtRo-uYmwRyFmOH3Xo4JDiP"
+                   "jioY7Ycna6ouhSSH0QcKsEjce10HCXIW_XtmEYr9SrB5BA").encode("utf-8")
+        coups = [coupl01, coupl02, coupl03]
+
+        key = dgKey(regk, vdig.qb64b)
+        assert issuer.getTibs(key) == []
+        assert issuer.cntTibs(key) == 0
+        assert issuer.delTibs(key) is False
+        assert issuer.putTibs(key, vals=[coupl01]) is True
+        assert issuer.getTibs(key) == [coupl01]
+        assert issuer.cntTibs(key) == 1
+        assert issuer.putTibs(key, vals=[coupl01]) is True  # add duplicate
+        assert issuer.cntTibs(key) == 1
+        assert issuer.addTib(key, coupl01) is False
+        assert issuer.addTib(key, coupl02) is True
+        assert issuer.cntTibs(key) == 2
+        assert issuer.putTibs(key, vals=[coupl02, coupl03]) is True
+        assert issuer.cntTibs(key) == 3
+        assert issuer.delTibs(key) is True
+        assert issuer.getTibs(key) == []
+        for c in coups:
+            assert issuer.addTib(key, c) is True
+        assert issuer.cntTibs(key) == 3
+        assert issuer.getTibs(key) == [coupl01, coupl02, coupl03]
+        for c in issuer.getTibsIter(key):
+            assert issuer.delTibs(key, c) is True
+        assert issuer.getTibs(key) == []
+
+        tweKey = snKey(regk, sn)
+        assert issuer.getTwe(tweKey) is None
+        assert issuer.delTwe(tweKey) is False
+        assert issuer.putTwe(tweKey, val=vdig.qb64b)
+        assert issuer.getTwe(tweKey) == vdig.qb64b
+        assert issuer.putTwe(tweKey, val=vdig.qb64b) is False
+        assert issuer.setTwe(tweKey, val=vdig.qb64b) is True
+        assert issuer.getTwe(tweKey) == vdig.qb64b
+        assert issuer.delTwe(tweKey) is True
+        assert issuer.getTwe(tweKey) is None
+
+        quad01 = ("DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo"
+                  "0AAAAAAAAAAAAAAAAAAAAABA"
+                  "rot"
+                  "Ezpq06UecHwzy-K9FpNoRxCJp2wIGM9u2Edk-PLMZ1H4").encode("utf-8")
+        quad02 = ("DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo"
+                  "0AAAAAAAAAAAAAAAAAAAAABg"
+                  "rot"
+                  "EOWdT7a7fZwRz0jiZ0DJxZEM3vsNbLDPEUk-ODnif3O0").encode("utf-8")
+        quad03 = ("DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo"
+                  "0AAAAAAAAAAAAAAAAAAAAABw"
+                  "rot"
+                  "EXvR3p8V95W8J7Ui4-mEzZ79S-A1esAnJo1Kmzq80Jkc").encode("utf-8")
+        quads = [quad01, quad02, quad03]
+
+        key = dgKey(regk, vdig.qb64b)
+        assert issuer.getAncs(key) == []
+        assert issuer.cntAncs(key) == 0
+        assert issuer.delAncs(key) is False
+        assert issuer.putAncs(key, vals=[quad01]) is True
+        assert issuer.getAncs(key) == [quad01]
+        assert issuer.cntAncs(key) == 1
+        assert issuer.putAncs(key, vals=[quad01]) is True  # add duplicate
+        assert issuer.cntAncs(key) == 1
+        assert issuer.addAnc(key, quad01) is False
+        assert issuer.addAnc(key, quad02) is True
+        assert issuer.cntAncs(key) == 2
+        assert issuer.putAncs(key, vals=[quad02, quad03]) is True
+        assert issuer.cntAncs(key) == 3
+        assert issuer.delAncs(key) is True
+        assert issuer.getAncs(key) == []
+        for c in quads:
+            assert issuer.addAnc(key, c) is True
+        assert issuer.cntAncs(key) == 3
+        assert issuer.getAncs(key) == [quad01, quad02, quad03]
+        for c in issuer.getAncsIter(key):
+            assert issuer.delAncs(key, c) is True
+        assert issuer.getAncs(key) == []
+
+        #  test with verifiable credential issuance (iss) event
+        vcdig = b'EXvR3p8V95W8J7Ui4-mEzZ79S-A1esAnJo1Kmzq80Jkc'
+        vck = vcKey(ipreb, regb, vcdig)
+        assert vck == (b'DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo:EOWdT7a7fZwRz0jiZ0DJxZEM3vsNbLDPEUk-ODnif3O0'
+                       b':EXvR3p8V95W8J7Ui4-mEzZ79S-A1esAnJo1Kmzq80Jkc')
+        sn = 0
+        vs = Versify(kind=Serials.json, size=20)
+
+        vcp = dict(v=vs, i=vck.decode("utf-8"),
+                   s="{:x}".format(sn),
+                   t="iss")
+
+        issb = json.dumps(vcp, separators=(",", ":"), ensure_ascii=False).encode("utf-8")
+        assert issb == (b'{"v":"KERI10JSON000014_",'
+                        b'"i":"DYmJApMvMb8mgiG20BPlPcKLWSfNIUCC21DP0i2_BLjo:'
+                        b'EOWdT7a7fZwRz0jiZ0DJxZEM3vsNbLDPEUk-ODnif3O0:EXvR3p8V95W8J7Ui4-mEzZ79S-A1esAnJo1Kmzq80Jkc",'
+                        b''b'"s":"0","t":"iss"}')
+        idig = Diger(ser=issb)
+
+        key = dgKey(vck, idig.qb64b)
+        assert issuer.getTvt(key) is None
+        assert issuer.delTvt(key) is False
+        assert issuer.putTvt(key, val=issb) is True
+        assert issuer.getTvt(key) == issb
+        assert issuer.putTvt(key, val=issb) is False
+        assert issuer.setTvt(key, val=issb) is True
+        assert issuer.getTvt(key) == issb
+        assert issuer.delTvt(key) is True
+        assert issuer.getTvt(key) is None
+
+        telKey = snKey(vck, sn)
+        assert issuer.getTel(telKey) is None
+        assert issuer.delTel(telKey) is False
+        assert issuer.putTel(telKey, val=idig.qb64b)
+        assert issuer.getTel(telKey) == idig.qb64b
+        assert issuer.putTel(telKey, val=idig.qb64b) is False
+        assert issuer.setTel(telKey, val=idig.qb64b) is True
+        assert issuer.getTel(telKey) == idig.qb64b
+        assert issuer.delTel(telKey) is True
+        assert issuer.getTel(telKey) is None
+
 
 
     """End Test"""
+
 
 if __name__ == "__main__":
     test_issuer()
